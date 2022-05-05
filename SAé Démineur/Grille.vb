70b1972846ad23018824a0367814eb81d08ce0ee
Ali-Thi <ali.talhaoui95@gmail.com>
2022-05-05 22:09:20 +0200

    ajout au module jeu buttonClick

--- SAé Démineur/Grille.vb
+++ SAé Démineur/Grille.vb
@@ -3,13 +3,11 @@
     Private tempsInitial As Integer = 60
     Private tempsRestant As Integer = tempsInitial
     Private labelTimer As New Label
+    Private nbRangees As Integer = Jeu.getNbRangees()
+    Private tailleBouton As Integer = 50
+    Private espacement As Integer = 10
 
     Private Sub Form_Load() Handles Me.Load
-        Dim nbRangees As Integer = Jeu.getNbRangees()
-
-        Dim tailleBouton As Integer = 50
-        Dim espacement As Integer = 10
-
         Dim posXOrigin As Integer = espacement
         Dim posYOrigin As Integer = espacement
         Dim posX As Integer = posXOrigin
@@ -26,17 +24,17 @@
         With labelTempsRestant
             .Text = "Temps restant : "
             .AutoSize = True
-            .Location = New Point(Me.Width / 2 - labelTempsRestant.Width / 2, espacement)
+            .Location = New Point(Me.Width \ 2 - labelTempsRestant.Width \ 2, espacement)
         End With
         With labelNomDuJoueur
             .Text = "Nom du joueur"
             .AutoSize = True
-            .Location = New Point(Me.Width / 2 - labelTempsRestant.Width / 2 - labelNomDuJoueur.Width, espacement)
+            .Location = New Point(Me.Width \ 2 - labelTempsRestant.Width \ 2 - labelNomDuJoueur.Width, espacement)
         End With
         With labelTimer
             .Text = tempsRestant
             .AutoSize = True
-            .Location = New Point(Me.Width / 2 + labelTempsRestant.Width / 2, espacement)
+            .Location = New Point(Me.Width \ 2 + labelTempsRestant.Width \ 2, espacement)
         End With
 
         Me.Size = New Drawing.Size(GroupBox1.Width + 10 * espacement, GroupBox1.Width + 10 * espacement)
@@ -47,9 +45,9 @@
             For j As Integer = 0 To nbRangees - 1
                 posX = (tailleBouton + espacement) * j + espacement
                 Dim btn As New Button
-                Dim lbl As New Label
+                'Dim lbl As New Label
 
-                GroupBox1.Controls.Add(lbl)
+                'GroupBox1.Controls.Add(lbl)
                 GroupBox1.Controls.Add(btn)
 
                 AddHandler btn.Click, AddressOf Button_Click
@@ -58,19 +56,20 @@
                     .Name = "Button" & Str(i * nbRangees + j + 1)
                     .Size = New Drawing.Size(tailleBouton, tailleBouton)
                     .Location = New Point(posX, posY)
+                    .BackColor = Color.DarkGray
                 End With
 
-                With lbl
-                    .Hide()
-                    '.Name = "Label" & Str(i * nbRangees + j + 1)
-                    If (Jeu.getGrilleI(i, j) <> 0) Then
-                        .Text = Jeu.getGrilleI(i, j)
-                    End If
-                    .Font = New Font(lbl.Font.FontFamily, tailleBouton / 2, FontStyle.Bold, lbl.Font.Unit)
-                    .TextAlign = ContentAlignment.MiddleCenter
-                    .Size = btn.Size
-                    .Location = New Point(btn.Left + tailleBouton / 2 - .Width / 2, btn.Top + tailleBouton / 2 - .Height / 2)
-                End With
+                'With lbl
+                '.Hide()
+                '.Name = "Label" & Str(i * nbRangees + j + 1)
+                'If (Jeu.getGrilleI(i, j) <> 0) Then
+                '.Text = Jeu.getGrilleI(i, j)
+                'End If
+                '.Font = New Font(lbl.Font.FontFamily, tailleBouton \ 2, FontStyle.Bold, lbl.Font.Unit)
+                '.TextAlign = ContentAlignment.MiddleCenter
+                '.Size = btn.Size
+                '.Location = New Point(btn.Left + tailleBouton \ 2 - .Width \ 2, btn.Top + tailleBouton \ 2 - .Height \ 2)
+                'End With
             Next
         Next
 
@@ -81,26 +80,47 @@
     End Sub
 
     Private Sub Button_Click(sender As Button, e As EventArgs)
-        For Each lbl In GroupBox1.Controls
-            If TypeOf (lbl) Is Label Then
-                If (lbl.Location.X >= sender.Location.X And lbl.Location.X <= sender.Location.X + sender.Width And lbl.Location.Y >= sender.Location.Y And lbl.Location.Y <= sender.Location.Y + sender.Height And lbl.Text <> "") Then
-                    If (CInt(lbl.Text) < 0) Then
-                        sender.BackColor = Color.Red
-                        Game_End()
-                    End If
-                    sender.Hide()
-                    lbl.Show()
-                End If
+        Dim casesADecouvrir() As Integer = Jeu.boutonClick(GroupBox1.Controls.IndexOf(sender))
+        If (casesADecouvrir IsNot Nothing) Then
+            If (casesADecouvrir(0) = 1) Then
+                For i As Integer = 1 To casesADecouvrir.Length - 1
+                    With GroupBox1.Controls(casesADecouvrir(i))
+                        Console.WriteLine(1)
+                        .Text = Jeu.getGrilleI(casesADecouvrir(i) \ nbRangees, casesADecouvrir(i) Mod nbRangees)
+                        .Font = New Font(.Font.FontFamily, tailleBouton \ 2, FontStyle.Bold, .Font.Unit)
+                        .BackColor = Color.LightGray
+                    End With
+                Next
+            Else
+                sender.BackColor = Color.Red
+                For i As Integer = 1 To casesADecouvrir.Length - 1
+                    Console.WriteLine(casesADecouvrir(i))
+                    With GroupBox1.Controls(casesADecouvrir(i))
+                        .Text = "B"
+                        .Font = New Font(.Font.FontFamily, tailleBouton \ 2, FontStyle.Bold, .Font.Unit)
+                    End With
+                Next
             End If
-
-        Next
+        End If
+        'For Each lbl In GroupBox1.Controls
+        'If TypeOf (lbl) Is Label Then
+        'If (lbl.Location.X >= sender.Location.X And lbl.Location.X <= sender.Location.X + sender.Width And lbl.Location.Y >= sender.Location.Y And lbl.Location.Y <= sender.Location.Y + sender.Height And lbl.Text <> "") Then
+        'If (CInt(lbl.Text) < 0) Then
+        'sender.BackColor = Color.Red
+        'Game_End()
+        'End If
+        'sender.Hide()
+        'lbl.Show()
+        'End If
+        'End If
+        'Next
     End Sub
 
     Private Sub Game_End()
         Me.Enabled = False
         Dim nbCaseDecouverte As Integer = 0
         For Each btn In GroupBox1.Controls
-            If (TypeOf (btn) Is Button And Not btn.Visible) Then
+            If (TypeOf (btn) Is Button And btn.Text <> "") Then
                 nbCaseDecouverte += 1
             End If
         Next
--- SAé Démineur/Jeu.vb
+++ SAé Démineur/Jeu.vb
@@ -1,9 +1,16 @@
 ﻿Module Jeu
     Private nbRangees As Integer = 8
-    Private nbMines As Integer = 10
+    Private nbMines As Integer = 1
     Private grille(nbRangees - 1, nbRangees - 1) As Integer
+    Private casesDecouvertes(nbRangees - 1, nbRangees - 1) As Boolean
 
     Public Sub MakeProblem()
+        For i As Integer = 0 To nbRangees - 1
+            For j As Integer = 0 To nbRangees - 1
+                grille(i, j) = 0
+                casesDecouvertes(i, j) = False
+            Next
+        Next
         VBMath.Randomize()
         Dim xAleatoire As Integer = CInt(VBMath.Rnd() * (nbRangees - 1))
         Dim yAleatoire As Integer = CInt(VBMath.Rnd() * (nbRangees - 1))
@@ -24,6 +31,52 @@
             Next
         Next
     End Sub
+
+    Public Function boutonClick(indexBouton As Integer) As Integer()
+        Dim x As Integer = indexBouton \ nbRangees
+        Dim y As Integer = indexBouton Mod nbRangees
+        Dim retour() As Integer
+        If (0 <= indexBouton < nbRangees * nbRangees) Then
+            If (Not casesDecouvertes(x, y)) Then
+                casesDecouvertes(x, y) = True
+                If (grille(x, y) < 0) Then
+                    retour = {0}
+                    For i As Integer = 0 To nbRangees - 1
+                        For j As Integer = 0 To nbRangees - 1
+                            If (grille(i, j) < 0) Then
+                                ReDim Preserve retour(retour.Length)
+                                retour.Append(i * nbRangees + j)
+                                casesDecouvertes(x, y) = True
+                            End If
+                        Next
+                    Next
+                ElseIf (grille(x, y) > 0) Then
+                    retour = {1, indexBouton}
+                    casesDecouvertes(x, y) = True
+                Else
+                    retour = {1}
+                    Dim retourRecursif() As Integer
+                    For Each i As Integer In {x - 1, x, x + 1}
+                        For Each j As Integer In {y - 1, y, y + 1}
+                            If (j >= 0 And i >= 0 And j < nbRangees And i < nbRangees And Not (j = y And i = x)) Then
+                                ReDim Preserve retour(retour.Length)
+                                retour.Append(i * nbRangees + j)
+                                If (Not casesDecouvertes(i, j)) Then
+                                    retourRecursif = boutonClick(i * nbRangees + j)
+                                    If (retourRecursif IsNot Nothing) Then
+                                        For k As Integer = 1 To retourRecursif.Length - 1
+                                            retour.Append(retourRecursif(k))
+                                        Next
+                                    End If
+                                End If
+                            End If
+                        Next
+                    Next
+                End If
+            End If
+        End If
+        Return retour
+    End Function
 
     Public Function getNbRangees() As Integer
         Return nbRangees

34534566e51a344b4ae9c0d724d06f0475618ee0
Sebastien Chen <sebastien.chen@etu.u-paris.fr>
2022-05-12 16:56:40 +0200

    Début de création de l'enregistrement

---
+++ SAé Démineur/Enregistrement.vb
@@ -0,0 +1,33 @@
+﻿Module Enregistrement
+    Structure joueur
+        Dim prenom As String
+        Dim mine As Integer
+        Dim nbParti As Integer
+        Dim temps As Integer
+    End Structure
+
+    Dim listJ As String = FreeFile()
+    Dim listJoueur As joueur()
+    Dim idxnbj As Integer = 0
+
+    Sub ajoutj(j As joueur)
+        FileOpen(listJ, "Enregistrement.txt", OpenMode.Output)
+        WriteLine(listJ, idxnbj)
+        WriteLine(listJ, j.prenom)
+        WriteLine(listJ, j.mine)
+        WriteLine(listJ, j.nbParti)
+        WriteLine(listJ, j.temps)
+        idxnbj += 1
+        FileClose(listJ)
+    End Sub
+
+
+
+    Public Function getListJ()
+        Dim linesFromFile, nextLine As String
+        Do Until EOF(listJ)
+            nextLine = LineInput(listJ)
+        Loop
+        Return listJoueur
+    End Function
+End Module
